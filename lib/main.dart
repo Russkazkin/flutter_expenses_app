825055862c659dc03d915efc91952538233eb3ed
Russkazkin <ruslan@skazkin.su>
2020-11-18 07:23:41 +0300

    Move widget structure for transaction list into new statefull  widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_expenses_app/widgets/transaction_list.dart';
 import 'package:intl/intl.dart';
-import 'models/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -62,51 +62,7 @@ class MyHomePage extends StatelessWidget {
                 ),
               ),
             ),
-            Column(
-              children: transactions
-                  .map((tx) => Card(
-                        child: Row(
-                          children: [
-                            Container(
-                              child: Text(
-                                '\$${tx.amount}',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 20,
-                                    color: Colors.purple),
-                              ),
-                              margin: EdgeInsets.symmetric(
-                                  vertical: 10, horizontal: 15),
-                              padding: EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                  border: Border.all(
-                                color: Colors.purple,
-                                width: 2,
-                              )),
-                            ),
-                            Column(
-                              children: [
-                                Text(
-                                  tx.title,
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Text(
-                                  DateFormat.yMMMd().format(tx.date),
-                                  style: TextStyle(
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                              ],
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                            ),
-                          ],
-                        ),
-                      ))
-                  .toList(),
-            )
+            TransactionList(),
           ],
         ),
       ),
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 
 class TransactionList extends StatefulWidget {
@@ -24,6 +25,50 @@ class _TransactionListState extends State<TransactionList> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: _userTransactions
+          .map((tx) => Card(
+        child: Row(
+          children: [
+            Container(
+              child: Text(
+                '\$${tx.amount}',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.purple),
+              ),
+              margin: EdgeInsets.symmetric(
+                  vertical: 10, horizontal: 15),
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.purple,
+                    width: 2,
+                  )),
+            ),
+            Column(
+              children: [
+                Text(
+                  tx.title,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  DateFormat.yMMMd().format(tx.date),
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+              crossAxisAlignment: CrossAxisAlignment.start,
+            ),
+          ],
+        ),
+      ))
+          .toList(),
+    );
   }
 }

87936adefb838981269d465d28fc2df6cd5b2a32
Russkazkin <ruslan@skazkin.su>
2020-11-18 07:40:21 +0300

    Add new statefulwidget to consolidate transactions logic and data

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'widgets/new_transaction.dart';
-import 'widgets/transaction_list.dart';
+
+import 'widgets/user_transactions.dart';
 
 void main() => runApp(MyApp());
 
@@ -33,8 +33,7 @@ class MyHomePage extends StatelessWidget {
               ),
               width: double.infinity,
             ),
-            NewTransaction(),
-            TransactionList(),
+            UserTransactions(),
           ],
         ),
       ),

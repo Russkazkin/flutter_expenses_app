0483db9b977f83a90fcfeccf9f44c462f54def6e
Russkazkin <ruslan@skazkin.su>
2020-11-26 07:35:00 +0300

    Add properties and methods into Chart widget

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
+import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
+  final List<Transaction> resentTransactions;
 
+  Chart(this.resentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(
+        7,
+        (index) => {
+              'day': 'T',
+              'amount': 9.99,
+            });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(children: [
-
-      ],),
+      child: Row(
+        children: [],
+      ),
     );
   }
 }

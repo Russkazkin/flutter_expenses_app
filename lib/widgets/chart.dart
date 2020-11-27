ca3baf181eabc6a021764a8de510176f28118d3a
Russkazkin <ruslan@skazkin.su>
2020-11-27 07:44:21 +0300

    Fix compilation errors

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -21,11 +21,11 @@ class Chart extends StatelessWidget {
         }
       }
 
-      print(DateFormat.E(weekDay));
+      print(DateFormat.E().format(weekDay));
       print(totalSum);
 
       return {
-        'day': DateFormat.E(weekDay),
+        'day': DateFormat.E().format(weekDay),
         'amount': totalSum,
       };
     });
@@ -33,6 +33,7 @@ class Chart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print(groupedTransactionValues);
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),

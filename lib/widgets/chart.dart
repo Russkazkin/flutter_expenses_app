dd2646aad9f3d3a9e83be62f9b06b6bb7d605708
Russkazkin <ruslan@skazkin.su>
2020-11-28 07:44:18 +0300

    Prepare ChartBar widget for really big numbers

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -39,16 +39,23 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: groupedTransactionValues
-            .map(
-              (data) => ChartBar(
-                label: data['day'],
-                spendingAmount: data['amount'],
-                spendingPctOfTotal: totalSpending == 0.0 ? 0.0 : (data['amount'] as double) / totalSpending,
-              ),
-            )
-            .toList(),
+      child: Padding(
+        padding: EdgeInsets.all(4),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues
+              .map(
+                (data) => Flexible(
+                  fit: FlexFit.tight,
+                  child: ChartBar(
+                    label: data['day'],
+                    spendingAmount: data['amount'],
+                    spendingPctOfTotal: totalSpending == 0.0 ? 0.0 : (data['amount'] as double) / totalSpending,
+                  ),
+                ),
+              )
+              .toList(),
+        ),
       ),
     );
   }

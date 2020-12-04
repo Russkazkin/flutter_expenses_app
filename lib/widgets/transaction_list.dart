5c82e1944584ddaa2fe896a1e3e22b7a77ffe6d5
Russkazkin <ruslan@skazkin.su>
2020-12-04 07:27:02 +0300

    Increase height of transactions list

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,7 +11,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 450,
+      height: 520,
       child: userTransactions.isEmpty
           ? Column(
               children: [

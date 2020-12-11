3d08d606010c1a2d45aa5682980bc057a5ff2700
Russkazkin <ruslan@skazkin.su>
2020-12-11 06:55:24 +0300

    Add property for input so it can take all height

--- lib/main.dart
+++ lib/main.dart
@@ -77,6 +77,7 @@ class _MyHomePageState extends State<MyHomePage> {
   void _startAddNewTransaction(BuildContext context) {
     showModalBottomSheet(
       context: context,
+      isScrollControlled: true,
       builder: (_) {
         return GestureDetector(
           child: NewTransaction(_addNewTransaction),

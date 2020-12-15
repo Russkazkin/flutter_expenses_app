3abc782ab80c2aa2f5b09a1ba92e79e00d6d44b0
Russkazkin <ruslan@skazkin.su>
2020-12-15 07:42:29 +0300

    Save MediaQuery call into variable

--- lib/main.dart
+++ lib/main.dart
@@ -94,7 +94,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
+    final mediaQuery = MediaQuery.of(context);
+    final isLandscape = mediaQuery.orientation == Orientation.landscape;
     final appBar = AppBar(
       title: Text('Personal Expenses'),
       actions: [
@@ -104,9 +105,9 @@ class _MyHomePageState extends State<MyHomePage> {
       ],
     );
     final txListWidget = Container(
-      height: (MediaQuery.of(context).size.height -
+      height: (mediaQuery.size.height -
           appBar.preferredSize.height -
-          MediaQuery.of(context).padding.top) *
+          mediaQuery.padding.top) *
           .75,
       child: TransactionList(_userTransactions, _deleteTransaction),
     );
@@ -132,17 +133,17 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             ),
             if(!isLandscape) Container(
-              height: (MediaQuery.of(context).size.height -
+              height: (mediaQuery.size.height -
                   appBar.preferredSize.height -
-                  MediaQuery.of(context).padding.top) *
+                  mediaQuery.padding.top) *
                   .25,
               child: Chart(_recentTransactions),
             ),
             if(!isLandscape) txListWidget,
             if(isLandscape) _showChart ? Container(
-              height: (MediaQuery.of(context).size.height -
+              height: (mediaQuery.size.height -
                       appBar.preferredSize.height -
-                      MediaQuery.of(context).padding.top) *
+                      mediaQuery.padding.top) *
                   .75,
               child: Chart(_recentTransactions),
             ) :

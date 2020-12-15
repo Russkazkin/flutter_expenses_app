3c2423296e8ade3a7d756b4c7f3c4d8285eb0086
Russkazkin <ruslan@skazkin.su>
2020-12-15 07:34:31 +0300

    Add optional button instead of icon for wide screens

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -53,14 +53,22 @@ class TransactionList extends StatelessWidget {
                 subtitle: Text(
                   DateFormat.yMMMd().format(userTransactions[index].date),
                 ),
-                trailing: IconButton(
-                  icon: Icon(
-                    Icons.delete_outline,
-                    color: Theme.of(context).errorColor,
-                  ),
-                  onPressed: () =>
-                      deleteTransaction(userTransactions[index].id),
-                ),
+                trailing: MediaQuery.of(context).size.width > 360
+                    ? FlatButton.icon(
+                        onPressed: () =>
+                            deleteTransaction(userTransactions[index].id),
+                        label: Text('Delete'),
+                        textColor: Theme.of(context).errorColor,
+                        icon: Icon(Icons.delete_outline),
+                      )
+                    : IconButton(
+                        icon: Icon(
+                          Icons.delete_outline,
+                          color: Theme.of(context).errorColor,
+                        ),
+                        onPressed: () =>
+                            deleteTransaction(userTransactions[index].id),
+                      ),
               ),
             ),
             itemCount: userTransactions.length,

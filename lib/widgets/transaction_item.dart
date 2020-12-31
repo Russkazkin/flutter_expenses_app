6d69950515b1cce2ea5c0e87494a480ee98caf25
Russkazkin <ruslan@skazkin.su>
2020-12-31 08:27:03 +0300

    Add background randomizer for transactions avatars

--- lib/widgets/transaction_item.dart
+++ lib/widgets/transaction_item.dart
@@ -1,8 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import '../models/transaction.dart';
 import 'package:intl/intl.dart';
 
-class TransactionItem extends StatelessWidget {
+import '../models/transaction.dart';
+
+class TransactionItem extends StatefulWidget {
   const TransactionItem({
     Key key,
     @required this.userTransaction,
@@ -13,42 +16,74 @@ class TransactionItem extends StatelessWidget {
   final Function deleteTransaction;
 
   @override
+  _TransactionItemState createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+  Color _bgColor;
+
+  @override
+  void initState() {
+    const availableColors = [
+      Colors.red,
+      Colors.deepOrange,
+      Colors.purple,
+      Colors.pinkAccent,
+    ];
+
+    _bgColor = availableColors[Random().nextInt(4)];
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
       elevation: 5,
       child: ListTile(
         leading: CircleAvatar(
+          backgroundColor: _bgColor,
           radius: 30,
           child: FittedBox(
             child: Padding(
               padding: const EdgeInsets.all(6.0),
-              child: Text('\$${userTransaction.amount}'),
+              child: Text('\$${widget.userTransaction.amount}'),
             ),
           ),
         ),
         title: Text(
-          userTransaction.title,
-          style: Theme.of(context).textTheme.headline6,
+          widget.userTransaction.title,
+          style: Theme
+              .of(context)
+              .textTheme
+              .headline6,
         ),
         subtitle: Text(
-          DateFormat.yMMMd().format(userTransaction.date),
+          DateFormat.yMMMd().format(widget.userTransaction.date),
         ),
-        trailing: MediaQuery.of(context).size.width > 360
+        trailing: MediaQuery
+            .of(context)
+            .size
+            .width > 360
             ? FlatButton.icon(
           onPressed: () =>
-              deleteTransaction(userTransaction.id),
+              widget.deleteTransaction(widget.userTransaction.id),
           label: const Text('Delete'),
-          textColor: Theme.of(context).errorColor,
+          textColor: Theme
+              .of(context)
+              .errorColor,
           icon: const Icon(Icons.delete_outline),
         )
             : IconButton(
           icon: Icon(
             Icons.delete_outline,
-            color: Theme.of(context).errorColor,
+            color: Theme
+                .of(context)
+                .errorColor,
           ),
           onPressed: () =>
-              deleteTransaction(userTransaction.id),
+              widget.deleteTransaction(widget.userTransaction.id),
         ),
       ),
     );
